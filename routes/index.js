82a9531dd6c93c1634834e0298282133a698b2aa
Stay Find <renzv231@gmail.com>
2026-03-30 16:10:55 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
-const express = require ('express');
-router = express.Router();
+const express = require('express');
+const router = express.Router(); // Added 'const' here
 const userController = require('../controllers/userController');
-//route to get all users
-router.get('/view', userController.getAllUsers);
-//Route to search ID
-router.get('/view/:id', userController.getUserById);
-//Route to create new user 
-router.post('/add', userController.createUser);
-// update
-router.put('/update', userController.updateUser);
-//router delete user
-router.delete('/delete', userController.deleteUser);
 
-module.exports=router;
+// All your routes below stay the same...
+router.get('/view', userController.getAllUsers);
+router.get('/view/:id', userController.getUserById);
+router.post('/add', userController.createUser);
+router.put('/update', userController.updateUser);
+router.delete('/delete', userController.deleteUser);
+router.post('/update-profile', userController.updateProfile);
+
+module.exports = router;
