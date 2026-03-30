bea8249433ac1bf0a1ad6029faf84ab2a782d44b
Stay Find <renzv231@gmail.com>
2026-03-30 16:37:52 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userControllers');
+const userController = require('../controllers/userController');
 
-router.get('/view', userControllers.getAllUsers);
-router.get('/view/:id', userControllers.getUserById);
-router.post('/add', userControllers.createUser);
-router.put('/update', userControllers.updateUser);
-router.delete('/delete', userControllers.deleteUser);
-router.post('/update-profile', userControllers.updateProfile);
+router.get('/view', userController.getAllUsers);
+router.get('/view/:id', userController.getUserById);
+router.post('/add', userController.createUser);
+router.put('/update', userController.updateUser);
+router.delete('/delete', userController.deleteUser);
+router.post('/update-profile', userController.updateProfile);
 
 module.exports = router;
