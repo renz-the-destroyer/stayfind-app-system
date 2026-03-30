4c883acabad904982b857ee0aafc286ffc3fc125
Stay Find <renzv231@gmail.com>
2026-03-30 16:09:27 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: 3306, 
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-module.exports = connection;
-
+module.exports = pool;
